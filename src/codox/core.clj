a779ff0e491052c598ae208863fe4956f20e53d2
Tibor Kranjcec <tibor@floatingpoint.io>
2020-07-19 11:40:29 +0200

    test codox

--- src/codox/core.clj
+++ src/codox/core.clj
@@ -1,1 +1,17 @@
-(ns codox.core)
+(ns codox.core
+  (:require [codox.main :refer [generate-docs]]))
+
+(defn build-docs
+  []
+  (generate-docs
+    {:name "frontend"
+     :version "0.1.0-SNAPSHOT"
+     :description "Frontend for the application."
+     :language :clojurescript
+     :source-paths ["src"]
+     :output-path "doc"
+     :root-path "."
+     :source-uri "https://our.git.server/dev/frontend/blob/{git-commit}/{filepath}#L{line}"
+     :metadata {:doc/format :markdown :doc "FIXME: Write documentation."}
+     :doc-paths ["doc"]
+     :themes [:dark]}))
